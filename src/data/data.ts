f9021fe204bf2978c501d83440c0089993f78c3f
CarlosColindres <70364536+CarlosColindres@users.noreply.github.com>
2021-02-10 03:03:22 -0500

    Update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -9,6 +9,15 @@ export const data = [
       'https://github.com/CarlosColindres/mike-store-front-end/tree/master/client',
   },
   {
+    img: 'https://i.ibb.co/0Kt6GVq/1612944048154.png',
+    name: 'Kanto Pokedex',
+    description:
+      'A pokemon pokedex built in next js and tailwind css. It has search functionality and page routing.',
+    live: 'https://pokemon-next-js.vercel.app/',
+    code:
+      'https://github.com/CarlosColindres/pokemon-next-js',
+  },
+  {
     img: 'https://i.ibb.co/b5YKPcf/1612824376427.png',
     name: 'Lambda School Projects',
     description:
@@ -16,13 +25,5 @@ export const data = [
     live: 'https://github.com/CarlosColindres?tab=repositories',
     code: 'https://github.com/CarlosColindres?tab=repositories',
   },
-  // {
-  //   img: 'https://i.ibb.co/vcTZm2B/1612464476751.png',
-  //   name: 'Nike e-Commerce Clone',
-  //   description:
-  //     'A full stack nike clone. Front-end built in Typescript, React, Redux, and Tailwind CSS.Back-end built in Typescript, Node, Express, Mongo DB with JWT Auth',
-  //   live: 'https://mike-store-gyyc1pun4.vercel.app/',
-  //   code:
-  //     'https://github.com/CarlosColindres/mike-store-front-end/tree/master/client',
-  // },
+  
 ]
